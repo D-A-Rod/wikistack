8354802d614f518c78a73bc3a463d3e30cca6470
Daniel Rodriguez <drodrigo95@hotmail.com>
2023-04-27 13:37:54 -0500

    server up and running

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const staticMiddleware = express.static((__dirname, "public"))
+const pg = require('pg')
+//const path = require('path')
+
+app.use(staticMiddleware)
+app.use(morgan('dev'))
+console.log('HELOO DAN');
+
+app.get('/', (req, res, next) => {
+    res.send("Heloo Dan!! This is your server ")
+})
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT} http://localhost:3000`);
+
+});
